12d10d4786e58ecfc69df3e895fbd48cdcfb6b83
JVolke <desire.add@gmail.com>
2017-05-27 03:42:36 +0200

    test

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -14,6 +14,7 @@ class ContentController extends Controller
         $itemColumns = [
             'itemDescription' => [
                 'name1',
+                'free3'
             ],
             'variationBase' => [
                 'id',
@@ -22,7 +23,7 @@ class ContentController extends Controller
             'variationRetailPrice' => [
                 'price'
             ],
-            
+
         ];
 
         $itemFilter = [
